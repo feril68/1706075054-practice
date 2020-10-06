297368fb78ffff68dbc02cae74e3c9bdd404e588
muhammad.feril <muhammad.feril@ui.ac.id>
2020-10-06 09:13:03 +0700

    add exercise2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 1706075054-practice
 
+website : https://practice-1706075054.herokuapp.com/
+
 Muhammad Feril Bagus Perkasa - 1706075054
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from random import randint
-
+from .models import Item
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
     context = {
-        "lucky_number":get_lucky_number()
+        "lucky_number":get_lucky_number(),
+        'items': Item.objects.all()
     }
     return render(request, 'home.html', context=context)
 
